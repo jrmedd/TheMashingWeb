a615064733a96b73e19870df86b01b315280104c
jrmedd <hello@jamesmedd.co.uk>
2015-12-28 15:03:42 +0000

    Start serial reading

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -86,28 +86,3 @@ $('button[name="B"]').on('click', function() {
     gameWin("B")
   };
 });
-
-function onGetDevices(ports){
-  if (ports.length > 0) {
-    $.each(ports, function(key, value) {
-      $('#serial-select').append($('<option></option>').attr('value', value.path).text(value.path));
-    });
-  }
-  else {
-    $('#serial-select').append($('<option disabled>No devices detected!</option>'));
-    $('#serial-select').prop('disabled', true);
-    $("#choose-serial-port").prop('disabled', true);
-  }
-};
-
-var connectionId = -1;
-
-function onConnect(connectionInfo){
-  connectionId = connectionInfo.connectionId;
-  console.log(connectionId);
-};
-
-$('#choose-serial-port').on('click', function(){
-  var selectedPort = $('#serial-select').val();
-  chrome.serial.connect(selectedPort, {bitrate: 9600}, onConnect);
-});
